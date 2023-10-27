dd66ce25687b68241ad64205dc55240e0b161d73
Andrey999K <andrey1234xxxx@mail.ru>
2023-10-27 07:45:01 +0300

    feat: edit url request on back

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiEndPoint: "http://localhost:8080/api"
+  apiEndPoint: process.env.NODE_ENV === "production" ? "http://95.143.188.184:8080/api" : "http://localhost:8080/api"
 };
 
 export default config;
