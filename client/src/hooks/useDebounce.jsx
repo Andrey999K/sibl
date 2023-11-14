8ddfcdf8302cb7a09faa21ffb8593a940883c8ed
Andrey999K <andrey1234xxxx@mail.ru>
2023-11-14 12:27:23 +0300

    feat: add custom hook useDebounce

--- client/src/hooks/useDebounce.jsx
+++ client/src/hooks/useDebounce.jsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import debounce from "../utils/debounce";
 
 const useDebounce = (cb, ms) => {
-  return useMemo(() => debounce(cb, ms), [ms]);
+  return useMemo(() => debounce(cb, ms), [ms, cb]);
 };
 
 export default useDebounce;
