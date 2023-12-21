fa756c11617efe4cda33b92c180ec28f1851a53a
Andrey999K <andrey1234xxxx@mail.ru>
2023-12-21 06:23:38 +0300

    test commit for course

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,21 +3,14 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: ["plugin:react/recommended", "standard", "plugin:storybook/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module"
   },
   plugins: ["react"],
-  ignorePatterns: [
-    "node_modules/",
-    "dist/",
-    "public/",
-    "build/",
-    "*.min.js",
-    "src/dev/"
-  ],
+  ignorePatterns: ["node_modules/", "dist/", "public/", "build/", "*.min.js", "src/dev/", "src/stories/"],
   rules: {
     semi: [1, "always"],
     "space-before-function-paren": "off",
